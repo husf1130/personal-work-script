01c48c2a4461158ad18e276ff214835dea1c4c8e
husf <hushoufeng@huawei.com>
2025-12-08 11:02:07 +0800

    Create d-run.sh

---
+++ pd-disaggregation/Qwe3-32B/d-run.sh
@@ -0,0 +1,21 @@
+#export MMC_LOCAL_CONFIG_PATH=/usr/local/memcache_hybrid/latest/config/mmc-local.conf
+export PYTHONPATH=${PWD}/sglang/python:$PYTHONPATH
+
+#PD
+export ASCEND_MF_STORE_URL="tcp://141.61.29.203:8000"
+
+python -m sglang.launch_server \
+    --model-path /data/Qwen3-32B \
+    --disaggregation-mode decode \
+    --host 141.61.29.203 \
+    --port 8081 \
+    --trust-remote-code \
+    --tp-size 2 \
+    --base-gpu-id 4 \
+    --mem-fraction-static 0.8 \
+    --attention-backend ascend \
+    --device npu \
+    --disaggregation-transfer-backend ascend \
+    --attention-backend ascend \
+    --max-running-requests 8 --context-length 3800 --chunked-prefill-size 57344 \
+    --disable-cuda-graph --disable-radix-cache
