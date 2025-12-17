eb3e6691c5b0a626ad2201b84437e610a786b8e0
husf <hushoufeng@huawei.com>
2025-12-17 11:44:53 +0800

    Update bench_cmd.sh

--- bench_cmd.sh
+++ bench_cmd.sh
@@ -8,3 +8,7 @@ python3 -m sglang.bench_serving \
     --gsp-system-prompt-len 3456 \
     --gsp-question-len 1 \
     --gsp-output-len 1
+
+export PYTHONPATH=/home/husf/sgl/sglang/python/:$PYTHONPATH
+    
+python3 -m sglang.bench_serving --backend sglang --host 127.0.0.1 --port 8899 --dataset-name random-ids --num-prompts 1 --max-concurrency 1 --random-input-len 1048576 --random-output-len 1024 --random-range-ratio 1 --request-rate 1
