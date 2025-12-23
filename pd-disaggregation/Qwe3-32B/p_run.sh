5222ac80db179657694dd007a725190790f8d6fb
husf <hushoufeng@huawei.com>
2025-12-23 15:19:50 +0800

    Update p_run.sh

--- pd-disaggregation/Qwe3-32B/p_run.sh
+++ pd-disaggregation/Qwe3-32B/p_run.sh
@@ -2,12 +2,13 @@
 export PYTHONPATH=${PWD}/sglang/python:$PYTHONPATH
 
 #PD
-export ASCEND_MF_STORE_URL="tcp://141.61.29.203:8000"
+export ASCEND_MF_STORE_URL="tcp://141.61.29.203:24000"
 
 python -m sglang.launch_server \
     --model-path /data/Qwen3-32B \
     --disaggregation-mode prefill \
     --disaggregation-bootstrap-port 8998 \
+    --disaggregation-transfer-backend ascend \
     --host 141.61.29.203 \
     --port 8080 \
     --trust-remote-code \
@@ -15,7 +16,6 @@ python -m sglang.launch_server \
     --mem-fraction-static 0.8 \
     --attention-backend ascend \
     --device npu \
-    --disaggregation-transfer-backend ascend \
     --attention-backend ascend \
     --max-running-requests 8 --context-length 3800 --chunked-prefill-size 57344 \
     --disable-cuda-graph --disable-radix-cache
